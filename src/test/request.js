89f7a87b086bae9b12a44ca4475f8a0baec6d8ac
Roman Mosevich <romos2606@gmail.com>
2021-11-29 07:37:51 +0300

    tests hotfix

--- src/test/request.js
+++ src/test/request.js
@@ -1,22 +1,24 @@
 const axios = require("axios");
 
+const url = "https://jogtracker.herokuapp.com/api/v1/test/echo";
+
 const getTest = async () => {
-    const response = await axios.get("https://jogtracker.herokuapp.com/api/v1/test/echo")
+    const response = await axios.get(url);
     return response;
 };
 
 const postTest = async () => {
-    const response = await axios.get("https://jogtracker.herokuapp.com/api/v1/test/echo")
+    const response = await axios.post(url);
     return response;
 };
 
 const putTest = async () => {
-    const response = await axios.get("https://jogtracker.herokuapp.com/api/v1/test/echo")
+    const response = await axios.put(url);
     return response;
 };
 
 const deleteTest = async () => {
-    const response = await axios.get("https://jogtracker.herokuapp.com/api/v1/test/echo")
+    const response = await axios.delete(url);
     return response;
 };
 
